48662d642c3b7f0918b394dc5ca845d979122d59
Gregor Vollmer <gregor@celement.de>
2013-07-12 01:28:09 +0200

    Fixed Comestible and Apple item classes

--- item/comestibles.py
+++ item/comestibles.py
@@ -4,10 +4,14 @@ from .itembase import Item, ItemStackable
 class Comestible(Item, ItemStackable):
     symbol = "%"
     
+    def __init__(self, game):
+        Item.__init__(self, game)
+        ItemStackable.__init__(self)
+
     def on_ingest(self):
         pass
 
-class Apple(Item, ItemStackable):
+class Apple(Comestible):
     name = "Apple"
     weight = 1
     value = 5
