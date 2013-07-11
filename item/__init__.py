526f9239e547293b85b30b610beda4d8ee0b406a
Gregor Vollmer <gregor@celement.de>
2013-07-12 01:43:43 +0200

    Added Item hotkey generation in Container
    Container now looks for unused keys and assing them to items prior to adding them.

--- item/__init__.py
+++ item/__init__.py
@@ -5,12 +5,28 @@ from item.itembase import ItemRegistry, Item, ItemStackable, ItemWieldable
 class Container:
     def __init__(self, game):
         self.items = []
-        
+
+    def get_hotkeys(self):
+        return [it.hotkey for it in self.items]
+
+    def get_unused_hotkey(self):
+        hotkeys = self.get_hotkeys()
+        ranges = [
+                  (ord("a"), ord("z")+1),
+                  (ord("A"), ord("Z")+1)
+                  ]
+        for r in ranges:
+            for i in range(*r):
+                ch = chr(i)
+                if ch not in hotkeys:
+                    return ch
+        return None
+
     def empty(self):
         i = list(map(lambda x: setattr(x, "container", None), self.items))
         self.items.remove(i)
         return i
-    
+
     def add(self, item):
         if isinstance(item, Container):
             item = Container.empty()
@@ -22,6 +38,7 @@ class Container:
             if i.container:
                 i.container.items.remove(i)
             i.container = self
+            i.hotkey = self.get_unused_hotkey()
             if isinstance(i, ItemStackable):
                 for it2 in self.items:
                     if it2.type == i.type:
